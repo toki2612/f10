1a3230b2bb021ebcc8a4c7fd1d2a50ffef7ac4a0
Evgeniya <evgeniya@codelane.ch>
2020-03-15 18:26:02 +0100

    more styles for discover

--- dcc/src/components/Discover.tsx
+++ dcc/src/components/Discover.tsx
@@ -7,8 +7,11 @@ import { Link } from 'react-router-dom';
 import { routerStore } from '../stores/routerStore';
 import { Slider, InputBase } from '@material-ui/core';
 import { observable, action } from 'mobx';
+import ReactCountryFlag from 'react-country-flag'
 import bind from 'bind-decorator';
 const Plus = require('react-ionicons/lib/IosAdd')
+const group = require('../resources/img/group.svg')
+const investment = require('../resources/img/payment.svg')
 
 interface IDiscoverProps {
 
@@ -49,6 +52,38 @@ export class Discover extends React.Component<IDiscoverProps> {
       )
     }
 
+    const icons: JSX.Element[] = []
+    for (const i of [5, 12, 17]) {
+      const n = i < 10 ? '0' + i : i
+      icons.push(
+        <div className={styles.iconBox}>
+          <img src={require(`../resources/sdg/E-WEB-Goal-${n}.png`)} alt={`sdg-${i}`}/>
+        </div>
+      )
+    }
+
+    const flags: JSX.Element = <div className={styles.flags}>
+      <ReactCountryFlag
+        countryCode={'CH'}
+        style={{
+            fontSize: '2em',
+            lineHeight: '2em',
+        }}/>
+        <ReactCountryFlag
+        countryCode={'US'}
+        style={{
+            fontSize: '2em',
+            lineHeight: '2em',
+        }}/>
+        <ReactCountryFlag
+        countryCode={'DE'}
+        style={{
+            fontSize: '2em',
+            lineHeight: '2em',
+        }}/>
+    </div>
+  
+
 
     return (
       <div className={styles.container}>
@@ -61,30 +96,48 @@ export class Discover extends React.Component<IDiscoverProps> {
             />
             <IconButton onClick={this.openSDG}><Plus color={'#fff'}/></IconButton>
           </div>
+        </div>
+        <div className={styles.scrollable}>
+          <div className={styles.icons}>{icons}</div>
           <div className={styles.sliders}>
-          <Slider
-            min={0}
-            max={5000}
-            value={this.employees}
-            onChange={this.handleEmployeesChange}
-            valueLabelDisplay="auto"
-            aria-labelledby="range-slider"
-            // getAriaValueText={valuetext}
-          />
-          <Slider
-            min={0}
-            max={500}
-            value={this.capital}
-            onChange={this.handleCapitalChange}
-            valueLabelDisplay="auto"
-            aria-labelledby="range-slider"
-            // getAriaValueText={valuetext}
-          />
+          <div className={styles.slider}>
+          <img src={investment} alt='investment-icon'/>
+          <div className={styles.val}>0</div>
+            <Slider
+              classes={{ rail: styles.rail, track: styles.rail }}
+              min={0}
+              max={5000}
+              value={this.employees}
+              onChange={this.handleEmployeesChange}
+              valueLabelDisplay="off"
+              aria-labelledby="range-slider"
+              // getAriaValueText={valuetext}
+            />
+            <div className={styles.val}>5000</div>
+          </div>
+          <div className={styles.slider}>
+            <img src={group} alt='group-icon'/>
+            <div className={styles.val}>0</div>
+            <Slider
+              classes={{rail: styles.rail, track: styles.rail}}
+              min={0}
+              max={500}
+              value={this.capital}
+              onChange={this.handleCapitalChange}
+              valueLabelDisplay="off"
+              aria-labelledby="range-slider"
+              // getAriaValueText={valuetext}
+            />
+            <div className={styles.val}>500</div>
+            </div>
+          </div>
+          {flags}
+          <div className={styles.videos}>
+          {videos}
           </div>
         </div>
-        <div className={styles.videos}>
-          {videos}
-        </div>
+        
+        
       </div>
     )
   }
